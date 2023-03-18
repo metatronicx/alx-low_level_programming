8d3ab3d674bc448464ca6fbf75a1bf900af9972d
Donald KANTI <donaldkant2000@gmail.com>
2023-03-18 18:26:49 +0100

    added some updates

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
-		printf("%d is 0\n", n);
+		printf("%d is zero\n", n);
 	if (n > 0)
-		printf("%d is greater than 0\n", n);
+		printf("%d is positive\n", n);
 	if (n < 0)
-		printf("%d is less than 0\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
