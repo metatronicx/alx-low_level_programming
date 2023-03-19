777864e490402e5e29a282f408a5882cd5b81ed4
Donald KANTI <donaldkant2000@gmail.com>
2023-03-19 17:46:51 +0100

    added some updates

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 0;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
