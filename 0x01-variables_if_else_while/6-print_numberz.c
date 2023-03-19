388d83bccc2f5497b199fcaac9a4a51b8b41d457
Donald KANTI <donaldkant2000@gmail.com>
2023-03-19 18:02:21 +0100

    added some updates

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x+'0');
+	}
+	putchar('\n');
+	return (0);
+}
