81c11b1315ce396b4f02622ac8a18a1f8d39f8ee
Donald KANTI <donaldkant2000@gmail.com>
2023-03-19 18:07:27 +0100

    added some updates

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x = 0;
+
+	for (x = 'Z'; x >= 'A'; x--)
+	{
+		putchar(tolower(x));
+	}
+	putchar('\n');
+	return (0);
+}
