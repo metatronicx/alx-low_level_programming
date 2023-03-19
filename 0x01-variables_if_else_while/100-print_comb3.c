b8b39251f63d094c20cdf28b1f9abd7585acd021
Donald KANTI <donaldkant2000@gmail.com>
2023-03-19 19:59:29 +0100

    added some updates

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,13 @@
 */
 int main(void)
 {
-	int i, j = 0;
+	int i = 0;
+	int j = 1;
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++) {
+		for (j = 1; j < 10; j++)
+		{
 			putchar(i + '0');
 			putchar(j + '0');
 			if (i < 8 || j < 9)
