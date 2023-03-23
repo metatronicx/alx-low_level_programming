bfc5946db8c153a7fa56334bbfc4cbe2ff3f92fa
Donald KANTI <donaldkant2000@gmail.com>
2023-03-23 23:21:03 +0100

    added password reset at first login

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - check the code
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	if (isupper(c))
+	{
+		return (1);
+	} 
+	return (0);
+
+}
