541433d8d84808e209f79697000404170968ac10
Donald KANTI <donaldkant2000@gmail.com>
2023-03-28 01:17:40 +0100

    added some updates

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,9 +10,9 @@
  *
  * Return: void
  */
-void swap_int(int *a, int *b) 
+void swap_int(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+int temp = *a;
+*a = *b;
+*b = temp;
 }
