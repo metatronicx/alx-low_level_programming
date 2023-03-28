1ac758c9bcff4fca61e75c21ce72ba1e9c412a05
Donald KANTI <donaldkant2000@gmail.com>
2023-03-28 01:30:23 +0100

    added some updates

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+/**
+ * _puts - Prints a string to stdout, followed by a new line
+ *
+ * @str: Pointer to the string to print
+ *
+ * Description:
+ * This function takes a pointer to a null-terminated string as parameter
+ * and prints the string, followed by a new line, to the standard output
+ * stream (stdout).
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+while (*str != '\0') {
+putchar(*str); 
+    str++;
+}
+
+putchar('\n');
+}
