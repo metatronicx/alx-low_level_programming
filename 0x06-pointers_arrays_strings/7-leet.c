15223cb1f92642d91f5c832343d207bea12ad40b
Donald KANTI <donaldkant2000@gmail.com>
2023-03-30 21:48:35 +0100

    added some updates

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+/**
+ * leet - Encodes a string into 1337.
+ *
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char leet[] = "4433007711";
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; letters[j] != '\0'; j++)
+{
+if (str[i] == letters[j])
+{
+str[i] = leet[j];
+}
+}
+}
+return (str);
+}
