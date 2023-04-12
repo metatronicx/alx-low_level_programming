c126953a4ea6c8b964d1c29b2be8532b2186fb8a
Donald KANTI <donaldkant2000@gmail.com>
2023-04-12 10:58:39 +0100

    made some updates

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 /**
- * create_array - Creates an array of chars, and initializes it with a specific char.
+ * create_array - Creates an array of chars, 
+ * and initializes it with a specific char.
  *
  * @size: The size of the array to be created.
  * @c: The char that the array should be initialized with.
