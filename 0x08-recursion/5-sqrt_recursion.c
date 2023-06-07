d6a7720408f591def7e37cf3f6c83f068d5192c5
Donald KANTI <donaldkant2000@gmail.com>
2023-06-08 00:10:28 +0100

    add some updates

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * _sqrt_recursion - Calculates the natural square root of a number recursively
  * @n: The number to find the square root of
@@ -8,27 +9,28 @@
  */
 int _sqrt_recursion(int n)
 {
-
-    int start = 0;
-    int end = n;
-    int mid = 0;
-    int square = 0;
+	int start = 1;
+	int end = n;
+	int mid = (start + end) / 2;        
     
-    if (n < 0)
-        return (-1); /* Negative number, no natural square root */
+        if (n < 0)
+		return (-1); /* Negative number, no natural square root */
 
-    while (start <= end)
-    {
-        mid = start + (end - start) / 2;
-        square = mid * mid;
+	if (n == 0 || n == 1)
+		return (n); /* Base case: square root is the number itself */
 
-        if (square == n)
-            return (mid); /* Found the natural square root */
-        else if (square > n)
-            end = mid - 1; /* Look in the lower half */
-        else
-            start = mid + 1; /* Look in the upper half */
-    }
 
-    return (-1); /* No natural square root found */
+	if (mid * mid == n)
+		return mid; /* Found the natural square root */
+	else if (mid * mid > n)
+		return (_sqrt_recursion(n / 2)); /* Look in the lower half */
+	else
+	{
+		int sub_result = _sqrt_recursion(n - mid);
+
+		if (sub_result == -1)
+			return (-1); /* No natural square root found */
+		else
+			return (mid + sub_result); /* Add the mid value */
+	}
 }
