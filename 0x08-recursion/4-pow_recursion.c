3739b496e429690c49730f6e6349a9b6a3a6deae
Donald KANTI <donaldkant2000@gmail.com>
2023-06-07 23:47:54 +0100

    add some updates

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 /**
- * _pow_recursion - Calculates the value of x raised to the power of y recursively
+ * _pow_recursion - Calculates the value of x 
+ * raised to the power of y recursively
  * @x: The base number
  * @y: The exponent
  *
- * Return: The value of x raised to the power of y, or -1 if an error occurs
+ * Return: The value of x raised to the power of y, 
+ * or -1 if an error occurs
  */
 int _pow_recursion(int x, int y)
 {
