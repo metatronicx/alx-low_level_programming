34c7a4a8421fc3aac537d61a39e7763d4dddf563
Donald KANTI <donaldkant2000@gmail.com>
2023-06-25 18:57:32 +0100

    added some change

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: Array of strings representing the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+
+return (0);
+}
