d2d3978db6b72ef9c86afafe0ab1acb0a78e4756
Donald KANTI <donaldkant2000@gmail.com>
2023-06-25 18:30:51 +0100

    added some change

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,14 +6,11 @@ int main(int argc, char *argv[])
     char *program_name = argv[0];
     char *name;
 
-    // Find the last occurrence of '/' to get the program name
     name = strrchr(program_name, '/');
     
     if (name == NULL) {
-        // No '/' found, use the whole program name
         name = program_name;
     } else {
-        // Include the path along with the program name
         name = program_name;
     }
 
